99eb76746324a0f40549f3750512bef886322430
renukamurthy1984re-cmd <renukamurthy1984re@gmail.com>
2026-09-04 18:24:59 +0530

    Update play_chess.py

--- play_chess.py
+++ play_chess.py
@@ -20,6 +20,8 @@ import ast
 import datetime
 from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
+import pandas as pd
+import sqlite3
 
 
 
@@ -663,6 +665,7 @@ if st.sidebar.button("Chessboard style to Normal", use_container_width=True):
 
 # --- INITIALIZE AUTHENTICATION & SESSION STATE ---
 
+
 USERS_FILE = "ataaa_users.json"
 
 def load_registered_users():
@@ -1048,6 +1051,7 @@ elif main_nav == "🚪 ATAAA for Chess Sign out page":
     else:
         st.info("⚠️ No accounts are currently signed in or registered on this device.")
     st.stop()
+
 
 # =========================================================================
 # TOP OPEN MATCH CONFIGURATION BAR (Engine Screen)
@@ -1914,7 +1918,7 @@ with col_nav2:
         st.rerun()        
     st.markdown("""
         <div style="margin-top: -60px; pointer-events: none; text-align: center;">
-            <p class="shimmer-btn-label">✈️ ATAAA AI - Hub For Learning</p>
+            <p class="shimmer-btn-label">GR</p>
         </div>
     """, unsafe_allow_html=True)
 
@@ -4280,7 +4284,6 @@ if st.session_state.get("show_ai_screen", False):
     if st.button("🔙 Back to engine screen", use_container_width=True, key="btn_back_to_engine"):
         st.session_state.show_ai_screen = False
         st.rerun()
-
 
 
 
